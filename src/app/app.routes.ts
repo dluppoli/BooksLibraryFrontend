1b48e03e95551e62bb169330c63f854466981305
Davide Luppoli <davide@davideluppoli.it>
2025-05-27 10:17:32 +0200

    Sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BookdetailComponent } from './components/bookdetail/bookdetail.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component:HomeComponent},
+    { path:'libri/:id', component:BookdetailComponent},
+    { path:'**', component:NotfoundComponent}
+];
---
+++ src/app/components/bookdetail/bookdetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-bookdetail',
+  imports: [],
+  templateUrl: './bookdetail.component.html',
+  styleUrl: './bookdetail.component.css'
+})
+export class BookdetailComponent {
+
+}
