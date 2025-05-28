560bd33df9231e920ed87cbf006468e0b0db408b
Davide Luppoli <davide@davideluppoli.it>
2025-05-28 11:54:13 +0200

    Sprint 2 - Environments

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Book } from '../models/Book';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,9 @@ export class BookService {
 
   constructor(private http:HttpClient) { }
 
-
-  private library:Book[] = [
-    { 
-      id:1, isbn:'123456',title:'Blue Tango',authors:['Paolo Roversi'],
-      categories:['Giallo'],image:'http://books.google.com/books/content?id=PJFEDwAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api',
-      description: 'lorem ipsum', stars:0, readingState:'Da Leggere'
-    },
-    { 
-      id:2, isbn:'78901234',title:'Eruption',authors:['James Patterson','Michael Crichton'],
-      categories:['Azione'],image:'http://books.google.com/books/content?id=o9AKEQAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api',
-      description: 'lorem ipsum', stars:3, readingState:'Da Leggere'
-    }
-  ]
-
   getAll(filtroAutore:string,filtroCategoria:string):Observable<Book[]>
   {
-    var url = 'https://bookslibrary-782617619332.europe-west1.run.app/libri'
+    var url = environment.apiUrl+'/libri';
 
     let filtri = "";
     if( filtroAutore!="-" && filtroAutore!="") filtri += `author=${filtroAutore}`
@@ -44,7 +31,7 @@ export class BookService {
 
   getOne(id:number):Observable<Book>
   {
-    return this.http.get<Book>('https://bookslibrary-782617619332.europe-west1.run.app/libri/'+id)
+    return this.http.get<Book>(environment.apiUrl+'/libri/'+id)
   }
 
   delete(id:number)
@@ -64,11 +51,11 @@ export class BookService {
 
   getAuthors():Observable<string[]>
   {
-    return this.http.get<string[]>('https://bookslibrary-782617619332.europe-west1.run.app/autori')
+    return this.http.get<string[]>(environment.apiUrl+'/autori')
   }
 
   getCategories():Observable<string[]>
   {
-    return this.http.get<string[]>('https://bookslibrary-782617619332.europe-west1.run.app/generi')
+    return this.http.get<string[]>(environment.apiUrl+'/generi')
   }
 }
